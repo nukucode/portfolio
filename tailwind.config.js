b1af76e3fa4fea41cd03af2336476f8f172e19f7
therogersak <therogersak@gmail.com>
2022-08-29 21:28:09 +0530

    productions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./layout/**/*.{js,ts,jsx,tsx}",
-    "./helpers/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.js",
+    "./components/**/*.js",
+    "./layout/**/*.js",
+    "./helpers/**/*.js",
     // Add more here
   ],
   darkMode: false, // or 'media' or 'class'
