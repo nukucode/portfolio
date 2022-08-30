961e8814fee51cee4de722eab850eebc44d9419b
therogersak <therogersak@gmail.com>
2022-08-30 06:24:45 +0530

    production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
