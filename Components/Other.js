9bfc2286f3d58abb383b42fb08b9734d4e531899
therogersak <therogersak@gmail.com>
2022-09-19 06:07:45 +0530

    error fix

--- Components/Other.js
+++ Components/Other.js
@@ -2,24 +2,13 @@ import React, { useEffect, useState } from "react";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import { WindowSharp } from "@mui/icons-material";
 function Other() {
-  const [btn, setBtn] = useState(false);
-
   useEffect(() => {
     const position = document.documentElement;
     document.body.addEventListener("mousemove", (e) => {
       position.style.setProperty("--L", e.clientX + "px");
       position.style.setProperty("--T", e.clientY + "px");
     });
-
-
-    if (typeof window !== 'undefined'){
-      window.addEventListener('scroll', () => {
-        if(this.window.pageYOffset >= 80){
-          setBtn(true)
-        }
-      })
-    }
-  },[]);
+  }, []);
 
   return (
     <>
@@ -32,7 +21,7 @@ function Other() {
           });
         }}
         className={`fixed bottom-5 right-5 bg-gray-800 w-10 h-10 rounded-full flex items-center justify-center transition-all duration-200 hover:bg-gray-900/70 hover:border-gray-900/20  animate-pulse ${
-          !btn ? "hidden" : "flex"
+          btn ? "hidden" : "flex"
         }`}
       >
         <KeyboardArrowUpIcon className="top" />
@@ -45,8 +34,6 @@ function Other() {
           alt="cursor-tracker"
         />
       </div>
-
-    
     </>
   );
 }
