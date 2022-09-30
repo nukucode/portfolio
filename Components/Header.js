664739afceb358c06e68397e786f816540aaae26
therogersak <therogersak@gmail.com>
2022-09-30 12:09:26 +0530

    :fire: remove code
    delete link from header section

--- Components/Header.js
+++ Components/Header.js
@@ -38,11 +38,7 @@ function Header() {
                   </li>
                 </Link>
 
-                <Link href="/skills">
-                  <li className=" cursor-pointer  transition-all duration-200">
-                    <a>Skills</a>
-                  </li>
-                </Link>
+            
 
                 <Link href="/projects">
                   <li className=" cursor-pointer  transition-all duration-200 ">
