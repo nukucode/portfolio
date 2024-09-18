6817f92aec9a1de02430b08e6a978cc8e71ca47c
Ankit Yadav <therogersak@gmail.com>
2024-09-18 14:31:57 +0530

    🔥 removed unused

--- client.js
+++
@@ -1,8 +0,0 @@
-// client.js
-import sanityClient from '@sanity/client'
-
-export default sanityClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECTID, 
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  useCdn: true 
-})
