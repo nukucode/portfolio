054764b0cb989eb374783bd18c824669f8c9a4df
Ankit Yadav <therogersak@gmail.com>
2024-09-18 14:32:28 +0530

    📦 add and remove package

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  swcMinify: true,
-  images: {
-    domains: [
-      "images.unsplash.com",
-      "cdn.sanity.io",
-      "avatars.githubusercontent.com",
-      "github.com",
-      "uploads-ssl.webflow.com",
-      "firebasestorage.googleapis.com",
-    ],
-  },
-
-  env: {
-    NEXT_PUBLIC_SANITY_DATASET: "production",
-    NEXT_PUBLIC_SANITY_PROJECTID: "1t6krb1s",
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
