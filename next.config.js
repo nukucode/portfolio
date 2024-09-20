d0f9c02de6fceafe7f6757a93b06c2c2af061799
Ankit Yadav <therogersak@gmail.com>
2024-09-20 21:52:29 +0530

    🔧 update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.dribbble.com"],
+    domains: ["cdn.dribbble.com", "github.com", "www.amplify-pr.co.uk"],
   },
 };
 
